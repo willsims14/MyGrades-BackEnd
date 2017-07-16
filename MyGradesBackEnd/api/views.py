81f266e16a92239430939481c3ec07b3558d14bb
William A Sims III <willsims14@gmail.com>
2017-07-16 09:31:03 -0500

    Fixed bug, POST now uses APIview

--- MyGradesBackEnd/api/views.py
+++ MyGradesBackEnd/api/views.py
@@ -56,7 +56,6 @@ class CourseList(viewsets.ModelViewSet):
     # Gets all courses for current user
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    print("\n\n\nHELLO11\n\n\n")
 
 
     def get_queryset(self):
@@ -70,28 +69,48 @@ class CourseList(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         student = Student.objects.get(user=self.request.user.id)
         semester = Semester.objects.get(pk=1)
-        # semester = Semester.objects.get(pk=self.req_body['semester'])
         print("\n\n\nHELLO2\n\n\n")
         serializer.save(student=student, semester=semester)
 
-    # def create(self, request):
-    #     # Look up objects by arbitrary attributes.
-    #     # You can check here if your students are participating
-    #     # the classes and have taken the subjects they sign up for.
-    #     semester = get_object_or_404(semester, title=request.data.get('semester'))
-    #     student = Student.objects.get(user=self.request.user.id)
-    #     # clazz = get_object_or_404(
-    #     #     Class, 
-    #     #     number=request.data.get('clazz_number')
-    #     #     letter=request.data.get('clazz_letter')
-    #     # )
+class CourseView(APIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save(student=student, semester=semester)
-    #     headers = self.get_success_headers(serializer.data)
+    def get(self, request, format=None):
+        courses = Course.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(student__user__username=username)
+        serializer = CourseSerializer(courses, context={'request': request}, many=True)
+        return Response(serializer.data)
 
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def post(self, request, format=None):
+        
+        req_body = json.loads(request.body.decode())
+        
+        semester_from_db = Semester.objects.get(pk=req_body['semester'])
+
+        new_course = Course.objects.create(
+            title = req_body['title'],
+            course_number = req_body['course_number'],
+            professor = req_body['professor'],
+            description = req_body['description'],
+            student = Student.objects.get(pk=request.user.id),
+            semester = semester_from_db
+        )
+
+        token = Token.objects.get(user=request.user)
+        data = json.dumps({'token':token.key})
+
+        try:
+            new_course.save()
+            return Response(data, content_type='application/json')
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 
 
